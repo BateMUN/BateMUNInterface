3b256c7343d44c0f7bd9ed5c556c41f932c6ab98
khalid-talakshi <khalid.talakshi@outlook.com>
2017-08-25 23:28:11 -0400

    Added comittee name and topic tags
    Added name and topic labels as well as coresponding functions to help change them

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//array to store all committees
-var committes = [committee1, committee2, committee3];
-
 //Committe 1 of the BateMUN Conference and all the relevant details
 var committee1 = {
     name: "Security Council",
@@ -22,6 +19,11 @@ var committee3 = {
     countries: ["Khalid", "Yash", "Aidan", "Shaheer", "Kira"]
 };
 
+//array to store all committees and committee index
+var committes = [committee1, committee2, committee3];
+var currentCommittee = 0;
+var currentTopic = 0;
+
 //different types of debates and a debate index to keep track of debates
 var debates = ["Formal Consideration", "Moderated Caucus", "Unmoderated Caucus", "Round Table", "For and Against", "Resolution Presentation", "Resolution voting", "other"];
 var currentDebate = 0;
@@ -31,10 +33,36 @@ function loadDebate() {
     document.getElementById("debateText").innerHTML = debates[currentDebate];
 }
 
+function loadCommitteeDetails() {
+    document.getElementById("committeeNameText").innerHTML = committee1.name;
+    document.getElementById("CommitteeTopicText").innerHTML = committee1.topics[0];
+}
+
+function changeCommitteeDetails() {
+    if (currentCommittee === committes.length - 1) {
+        currentCommittee = 0;
+    } else {
+        currentCommittee++;
+    }
+    console.log(currentCommittee);
+    console.log(committes.length);
+    document.getElementById("committeeNameText").innerHTML = committes[currentCommittee].name;
+    document.getElementById("committeeTopicText").innerHTML = committes[currentCommittee].topics[0];
+}
+
+function changeCommitteeTopic() {
+    if (currentTopic === committes[currentCommittee].topics.length - 1) {
+        currentTopic = 0;
+    } else {
+        currentTopic++;
+    }
+    document.getElementById("committeeTopicText").innerHTML = committes[currentCommittee].topics[currentTopic];
+}
+
 //changes the debate
 //if the debate is greater then the number of debates possible, set debate index to 0, otherwise increase debate index and
 function changeDebate () {
-    if (currentDebate == (debates.length - 1)) {
+    if (currentDebate > debates.length) {
         currentDebate = 0;
     } else {
         currentDebate++;
